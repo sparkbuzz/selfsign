86ee22043e391db50098e5e6b7f841637402aa09
Josef van Niekerk <josef.van.niekerk@gmail.com>
2018-03-28 09:09:13 +0200

    Use fictional data, split config, output data in working dir

--- config.bash
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-OUT_PATH=/tmp
-
-COUNTRY_CODE="ZA"
-LOCALITY="Cape Town"
-STATE="Western Cape"
-COMPANY="ACME"
-COMMON_NAME="acme.com"
---
+++ config/ssl.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+COUNTRY_CODE="UT"
+LOCALITY="Utopia"
+STATE="State of Utopia"
+COMPANY="ACME"
+COMMON_NAME="acme.localhost"
